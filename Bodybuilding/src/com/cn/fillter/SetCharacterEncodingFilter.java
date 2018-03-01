031df57544c58ec594a737361ccaacb330bc3f96
CliuGeek9229 <630877594@qq.com>
2018-03-01 09:40:31 +0800

    添加Web框架大作业代码

---
+++ Bodybuilding/src/com/cn/fillter/SetCharacterEncodingFilter.java
@@ -0,0 +1,39 @@
+package com.cn.fillter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SetCharacterEncodingFilter implements Filter{
+	protected String encoding = null;
+	protected FilterConfig filterConfig = null;
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding(encoding);
+		response.setCharacterEncoding(encoding);
+		
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		this.filterConfig = filterConfig;
+		this.encoding = filterConfig.getInitParameter("encoding");
+		
+	}
+
+}
